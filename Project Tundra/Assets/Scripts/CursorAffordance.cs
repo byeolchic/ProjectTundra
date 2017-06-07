4213163ececfe16c2ab789e4ca759ddd4bd0974e
donnyevason <donny.evason@gmail.com>
2017-06-07 20:21:27 +0100

    First Commit
    First Game Test Commit - Likely master for now

---
+++ Project Tundra/Assets/Scripts/CursorAffordance.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent (typeof(CameraRaycaster))]
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D walkCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+	[SerializeField] Texture2D targetCursor = null;
+	[SerializeField] Vector2 cursorHotspot = new Vector2 (0, 0);
+
+
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster>();
+        cameraRaycaster.OnLayerChange += OnlayerChanged; // registering 
+		
+	}
+	
+	// Only called when layer changes
+	void OnlayerChanged (Layer newLayer) {
+        print("Cursor over new layer");
+		switch (newLayer)
+		{
+			case Layer.Walkable:
+				Cursor.SetCursor(walkCursor, cursorHotspot, CursorMode.Auto); //Auto = hardware rendering of cursor - ForceSoftware might fix this if needed
+				break;
+			case Layer.RaycastEndStop:
+				Cursor.SetCursor(unknownCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.Enemy:
+				Cursor.SetCursor(targetCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			default:
+				Debug.LogError("Don't know what cursor to show!");
+				return;
+		}
+	}
+}
